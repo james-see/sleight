df2ff3f0e3f2175ef00f98abe6994263ff99ff00
James Campbell <james@jamescampbell.us>
2026-08-29 16:48:03 -0400

    feat: full-screen window by default, content-size resizability

--- sleight/App/SleightApp.swift
+++ sleight/App/SleightApp.swift
@@ -6,7 +6,11 @@ struct SleightApp: App {
         WindowGroup("Sleight") {
             ContentView()
                 .frame(minWidth: 960, minHeight: 640)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
         .windowToolbarStyle(.unified)
+        .defaultSize(width: NSScreen.main?.frame.width ?? 1440,
+                     height: NSScreen.main?.frame.height ?? 900)
+        .windowResizability(.contentSize)
     }
 }
